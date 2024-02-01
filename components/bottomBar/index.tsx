0097d354887adaf5174ed035b807ecf09425febb
TakamiInagaki <takami.l.inagaki@gmail.com>
2024-01-31 22:58:18 -0800

    bottom bar first version

--- components/bottomBar/index.tsx
+++ components/bottomBar/index.tsx
@@ -1,32 +1,25 @@
 import Image from "next/image"
-import styles from "../../components/topBar/TopBar.module.css"
-import Link from "next/link"
+import styles from "../../components/bottomBar/BottomBar.module.css"
+
+
+import home from "../../public/images/Home.svg"
+import genres from "../../public/images/Folder.svg"
+import music from "../../public/images/music.svg"
+import map from "../../public/images/World.svg"
+import calender from "../../public/images/Flame.svg"
+
+
 
 import genresIcon from "../../public/images/genresIcon.svg"
 export default function BottomBar(){
     return(
         <>
-            <main className={styles.topBarBody}>
+            <main className={styles.bottomBarBody}>
 
-                <Link href="/genres">
-                    <Image 
-                        className={styles.icon}
-                        src={genresIcon}
-                        alt=""
-                        width={30}
-                        height={30}
-                    />
-                    <p>Genres</p>
-                </Link>
+                <a className={styles.bottomBarItem} href="/genes"><Image src={genres} alt="" height={40} width={40}/>Genres</a>
+                <a className={styles.bottomBarItem} href="/"><Image src={home} alt="" height={40} width={40}/>Home</a>
+                <a className={styles.bottomBarItem} href="/artists"><Image src={music} alt="" height={40} width={40}/>Artists</a>
                 
-                {/* <h1>{pageName}</h1>
-                    
-                <Image 
-                    src={dropDownMenuIcon}
-                    alt = ""
-                    height={30}
-                    width={40} */}
-                {/* /> */}
             </main>  
         </>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import BottomBar from "../../components/bottomBar"
-import TopBar from "../../components/topBar"
+
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}>
   
-    <TopBar />
+    <BottomBar />
 
     </main>
   )
