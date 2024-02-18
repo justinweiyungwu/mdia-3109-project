def57ca19a567039dfa99dae56d61b1c0fe5cb1e
TakamiInagaki <takami.l.inagaki@gmail.com>
2024-02-18 13:02:12 -0800

    components for desktop

---
+++ components/genres/index.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import styles from "../genres/GenresCard.module.css"
+
+export default function GenresCard({
+    songName="",
+    singer = "",
+    link = "",
+    image = "",
+}){
+    return(
+        <>
+            <main className={styles.body}>
+             <a href={link} target="_blank">
+                <Image 
+                    className={styles.genresImage}
+                    src={image}
+                    alt=""
+                    width={175}
+                    height={175}/>
+
+                 <div className={styles.genresDetails}>
+                    <h1 className={styles.songName}>{songName}</h1>
+                    <h2 className={styles.singer}>{singer}</h2>
+                 </div>
+             </a>
+            </main>
+        </>
+    )
+}
