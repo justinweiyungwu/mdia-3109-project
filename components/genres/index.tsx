b65954567f930390127be40bba87ed218d84908c
Jacob Skilling <113203264+JacobSkilling-dot@users.noreply.github.com>
2024-02-20 03:48:57 +0000

    Added album covers and genre page changes

--- components/genres/index.tsx
+++ components/genres/index.tsx
@@ -1,29 +1,32 @@
-import Image from "next/image"
-import styles from "../genres/GenresCard.module.css"
+import TopBar from "../topBar";
+import BottomBar from "../bottomBar";
+import Image from "next/image";
+import styles from "../genres/GenresCard.module.css";
 
 export default function GenresCard({
-    songName="",
-    singer = "",
-    link = "",
-    image = "",
-}){
-    return(
-        <>
-            <main className={styles.body}>
-             <a href={link} target="_blank">
-                <Image 
-                    className={styles.genresImage}
-                    src={image}
-                    alt=""
-                    width={175}
-                    height={175}/>
+  songName = "I KNOW ?",
+  singer = "Travis Scott",
+  link = "",
+  image = "",
+}) {
+  return (
+    <>
+      <main className={styles.body}>
+        <a href={link} target="_blank">
+          <Image
+            className={styles.genresImage}
+            src={image}
+            alt=""
+            width={175}
+            height={175}
+          />
 
-                 <div className={styles.genresDetails}>
-                    <h1 className={styles.songName}>{songName}</h1>
-                    <h2 className={styles.singer}>{singer}</h2>
-                 </div>
-             </a>
-            </main>
-        </>
-    )
+          <div className={styles.genresDetails}>
+            <h1 className={styles.songName}>{songName}</h1>
+            <h2 className={styles.singer}>{singer}</h2>
+          </div>
+        </a>
+      </main>
+    </>
+  );
 }
