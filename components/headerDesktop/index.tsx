d427e78d6827490f8bbdf6d3b77306feda607c66
justinweiyungwu <justinweiyungwu@gmail.com>
2024-02-21 09:41:59 -0800

    removing components, creating desktop header
    wed feb 21
    justin wu

---
+++ components/headerDesktop/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import styles from "../../components/topBar/TopBar.module.css"
+import { useState } from "react"
+import { useRouter } from "next/router"
+
+import searchIcon from "../../public/images/search.svg"
+
+import home from "../../public/images/Home.svg"
+import genres from "../../public/images/Folder.svg"
+import music from "../../public/images/music.svg"
+import map from "../../public/images/World.svg"
+import calender from "../../public/images/Flame.svg"
